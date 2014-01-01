31b1eb5485a43fb016d6437bf5949ec6584fb1d1
M42 <mikrokosmos42@gmail.com>
2014-01-01 01:15:12 +0100

    Lectura de imágenes. Adaptando la implementación de la lectura de imagenES. Funciones de lectura de cabecera, salto de comentarios y volcado de imágenes.

--- include/imagen.h
+++ include/imagen.h
@@ -1,6 +1,9 @@
 #ifndef _IMAGEN_H
 #define _IMAGEN_H
 
+#include <iostream>
+#include <limits>
+
 /**
  * Pixel de una imagen.
  * Contiene información del color del pixel y transparencia.
@@ -38,6 +41,21 @@ private:
 	IMAGEN_PPM
     };
 
+
+    /**
+     * Buffer con información en bruto de la imagen.
+     * Información no interpretada de la imagen.
+     */
+    typedef unsigned char* Buffer;
+
+
+    /**
+     * Tamaño máximo permitido a una imagen.
+     * Máximo de filas y máximo de columnas.
+     */
+    static const int TAMANIO_MAXIMO = 5000;
+
+    
     /**
      * @brief Consulta el tipo de imagen del archivo y sus dimensiones
      *
@@ -47,6 +65,58 @@ private:
      * @see TipoImagen
      */
     static TipoImagen leerTipoImagen (istream& input);
+
+
+    /**
+     * @brief Lee la cabecera de una imagen PPM o PGM.
+     * @param input Flujo de entrada de la imagen.
+     * @param filas Número de filas de la imagen según la cabecera.
+     * @param columnas Número de columnas de la imagen según la cabecera.
+     * @return Verdadero si la lectura tuvo éxito.
+     */
+    static bool LeerCabecera (istream& input, int& filas, int& columnas)
+
+    /**
+     * @brief Lee una imagen de tipo PPM reservando la memoria necesaria.
+     *
+     * @param input Flujo de entrada de la imagen.
+     * @param filas Parámetro de salida con las filas de la imagen.
+     * @param columnas Parámetro de salida con las columnas de la imagen.
+     * @return Buffer con la imagen en bruto. 
+     * El buffer será una zona de memoria para obtener el valor de cada uno de los píxeles
+     * como tripletas consecutivas en formato RGB (RGBRGBRGB...) por filas
+     * desde la esquina superior izquierda a la inferior derecha.
+     */
+    static Buffer leerImagenPPM (istream& input, int& filas, int& columnas);
+    
+    /**
+     * @brief Lee una imagen de tipo PGM reservando la memoria necesaria.
+     *
+     * @param input Flujo de entrada de la imagen.
+     * @param filas Parámetro de salida con las filas de la imagen.
+     * @param columnas Parámetro de salida con las columnas de la imagen.
+     * @return Buffer con la imagen en bruto. 
+     * El buffer será una zona de memoria para obtener el valor de cada uno de los píxeles
+     * como un valor de grises por filas desde la esquina superior izquierda a la inferior derecha.
+     */
+    static Buffer leerImagenPGM (istream& input, int& filas, int& columnas);
+
+
+    /**
+     * Salta las líneas de comentarios del flujo de entrada.
+     * Retirará las líneas que comiencen por # hasta encontrar una que no lo haga.
+     * @param input Flujo de entrada.
+     */
+    static void saltarComentarios (istream& input)
+
+    /**
+     * Salta los separadores del flujo de entrada.
+     * Retirará separadores del flujo hasta encontrar un caracter no separador.
+     * @param input Flujo de entrada de los caracteres.
+     * @return Caracter no separador.
+     */
+    static char saltarSeparadores (istream& input);
+    
 
 public:
     friend istream& operator >> (istream& input, Imagen& leida);
--- src/imagen.cpp
+++ src/imagen.cpp
@@ -6,7 +6,7 @@ istream& operator >> (istream& input, Imagen& leida) {
 }
 
 
-TipoImagen LeerTipoImagen (istream& input) {
+TipoImagen Imagen::leerTipoImagen (istream& input) {
     /**
      * El tipo de imagen viene dado por los dos primeros caracteres.
      * El primer caracter debe ser una P y el segundo debe ser un número.
@@ -30,4 +30,75 @@ TipoImagen LeerTipoImagen (istream& input) {
 
     return res;
 
+}
+
+
+Buffer Imagen::leerImagenPPM (istream& input, int& filas, int& columnas) {
+    // Si puede leer la cabecera correctamente,
+    // reserva espacio para el buffer y lo vuelca directamente.
+    if (LeerCabecera (input, filas, columnas)) {
+	int tamanio_buffer = filas * columnas * 3;
+	Buffer buffer = new char [tamanio_buffer]; 
+    
+	if (input.read(buffer, tamanio_buffer))
+	    return buffer;
+    }
+
+    // Caso de lectura errónea.
+    filas = 0;
+    columnas = 0;
+    return NULL;
+}
+
+Buffer Imagen::leerImagenPGM (istream& input, int& filas, int& columnas) {
+    // Si puede leer la cabecera correctamente,
+    // reserva espacio para el buffer y lo vuelca directamente.
+    if (LeerCabecera (input, filas, columnas)) {
+	int tamanio_buffer = filas * columnas;
+	Buffer buffer = new char [tamanio_buffer]; 
+    
+	if (input.read(buffer, tamanio_buffer))
+	    return buffer;
+    }
+  
+    // Caso de lectura errónea.
+    filas = 0;
+    columnas = 0;
+    return NULL;
+}
+
+
+
+bool Imagen::LeerCabecera (istream& input, int& filas, int& columnas) {
+    int maxvalor;
+    saltarComentarios(input);
+    input >> columnas >> filas >> maxvalor;
+    
+    if (input and filas > 0 and filas < 5000 and columnas > 0 and columnas < 5000) {
+        f.get();
+        return true;
+    }
+    
+    return false;
+}
+
+
+void Imagen::saltarComentarios (istream& input) {
+    // Salta todas las líneas que comiencen con #.
+    static const int SALTO_MAXIMO = numeric_limits<streamsize>::max();
+    while (saltarSeparadores(input) == '#')
+	input.ignore (TAMANIO_SALTO, '\n');
+}
+
+
+char Imagen::saltarSeparadores (istream& input) {
+    // Extrae caracteres hasta que alguno sea distinto a un espacio.
+    char leido;
+
+    do
+	leido = input.get();
+    while (isspace(leido));
+    input.putback(leido);
+
+    return leido;
 }
