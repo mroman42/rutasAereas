bcbace2ec609c7af9035ff27c604b89df3289ea6
oscarbg <oscarbg@oscarbg-Lenovo-G580.(none)>
2014-01-18 12:35:27 +0100

    Pegado de banderas
    Para cada país, unimos la dirección del directorio de las banderas
    al nombre de la bandera del país.
    Leemos dicho archivo y pegamos las banderas en el mapa.

--- src/main.cpp
+++ src/main.cpp
@@ -119,18 +119,36 @@ int main(int argc, char * argv[]){
     int total_columnas = mapa.numColumnas();
     int total_filas    = mapa.numFilas();
 
+    char * direccion_bandera;
+    direccion_bandera = new char[1000];
+    Imagen bandera;
+
     for (rit i = ruta.begin(); i != ruta.end(); ++i) {
         Pais& pais = paises[*i];
         int pos_columnas = (total_columnas/360.0) * (180 + i->Longitud());
         int pos_filas = (total_filas/180.0) * (90 - i->Latitud());
 
-        // Pegado de bandera.
-        // ifstream archivo_bandera (pais.bandera);
-        // Imagen bandera;
-        // archivo_bandera >> bandera;
-        // archivo_bandera.close();
 
-        // mapa.pega (bandera, VACIA, true, pos_filas, pos_columnas);
+        // Pegado de bandera.
+        strcpy(direccion_bandera, argv[3]);
+        strcat(direccion_bandera, "/");
+        strcat(direccion_bandera, pais.bandera.c_str());
+
+        cout << direccion_bandera << endl;
+
+
+        ifstream archivo_bandera (direccion_bandera);
+        if (!archivo_bandera) {
+            cerr << "Error al abrir el archivo " << direccion_bandera << endl;
+            return -1;
+        }
+        archivo_bandera >> bandera;
+
+        archivo_bandera.close();
+
+        cout << bandera;
+
+        mapa.pega (bandera, VACIA, false, pos_filas, pos_columnas);
     }
 
     //Creamos y abrimos el archivo de salida y grabamos la imagen
